e529f11dc1e52c948476a225f0d2bb5ef288c6de
silviacremascoli <silcremascoli@gmail.com>
2023-04-26 10:57:49 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        <p>
+          This project was coded by{" "}
+          <a href="https://responsive-portfolio-cremascoli.netlify.app/">
+            Silvia Cremascoli
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/silviacremascoli/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          🚀
+        </p>
+      </footer>
     </div>
   );
 }
