38708cc18fea6bfd0a04e369ea9587d1808ff274
silviacremascoli <silcremascoli@gmail.com>
2023-04-26 12:47:44 +0200

    Added basic CSS
    Just temporary, going to change it later

--- src/Weather.js
+++ src/Weather.js
@@ -12,6 +12,7 @@ export default function Weather() {
               name="search"
               placeholder="Type a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
@@ -19,27 +20,30 @@ export default function Weather() {
               type="submit"
               name="submit"
               value="Search"
-              className="btn btn-success"
+              className="btn border btn-light w-100"
             />
           </div>
         </div>
       </form>
       <h1>Auckland</h1>
       <ul>
-        <li>26th April, Wednesday, 11:20</li>
+        <li>Wednesday, 26th April</li>
         <li>Sunny</li>
       </ul>
       <div className="row">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Sunny"
-          />
-          11°C
+          <div className="d-flex justify-content-start">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Sunny"
+            />
+            <span className="temperature">11</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
-        <div className="col-6">
+        <div className="col-6 p-2">
           <ul>
-            <li>Precipitation: </li>
+            <li>Current time: 11:20 AM</li>
             <li>Humidity: 60%</li>
             <li>Wind speed: 4 km/h</li>
           </ul>
